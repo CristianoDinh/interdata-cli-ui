6ae3d2788560d87ab56ddb999d40bbddd09c865f
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-03-13 00:33:46 +0700

    update url

--- src/app/services/s3.service.ts
+++ src/app/services/s3.service.ts
@@ -10,18 +10,19 @@ import {S3Object} from '../interface/S3Object';
 
 
 export class S3Service {
-  private apiUrl = 'http://127.0.0.1:5000';
+  private apiUrlLocal = 'http://127.0.0.1:5000';
+  private apiUrlServer = 'https://dinhgiabao.io.vn';
   //Phiên bản Angular 14 trở về trước:  constructor( private http: HttpClient ) { }
   http = inject(HttpClient);
 
   //1. Gọi API get List buckets
   getBuckets(): Observable<S3Bucket[]> {
-    return this.http.get<S3Bucket[]>(`${this.apiUrl}/buckets`);
+    return this.http.get<S3Bucket[]>(`${this.apiUrlServer}/buckets`);
   }
 
   //2. Gọi API get list objects IN bucket
   getBucketObjects(bucketName: string) :Observable<S3Object[]> {
-    return this.http.get<S3Object[]>(`${this.apiUrl}/${bucketName}/objects`);
+    return this.http.get<S3Object[]>(`${this.apiUrlServer}/${bucketName}/objects`);
   }
 
 }
