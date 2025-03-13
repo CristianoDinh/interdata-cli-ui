5e7c467557b07f4371b9f81b92be012e33720ae8
CristianoDinh <dinhgiabao1142004@gmail.com>
2025-03-13 16:19:18 +0700

    update api url

--- src/app/services/s3.service.ts
+++ src/app/services/s3.service.ts
@@ -11,18 +11,19 @@ import {S3Object} from '../interface/S3Object';
 
 export class S3Service {
   private apiUrlLocal = 'http://127.0.0.1:5000';
-  private apiUrlServer = 'https://interdata-cli-api.vercel.app';
+  private apiUrlVercel = 'https://interdata-cli-api.vercel.app';
+  private apiUrlVPS = 'http://103.20.96.236';
   //Phiên bản Angular 14 trở về trước:  constructor( private http: HttpClient ) { }
   http = inject(HttpClient);
 
   //1. Gọi API get List buckets
   getBuckets(): Observable<S3Bucket[]> {
-    return this.http.get<S3Bucket[]>(`${this.apiUrlServer}/buckets`);
+    return this.http.get<S3Bucket[]>(`${this.apiUrlVPS}/buckets`);
   }
 
   //2. Gọi API get list objects IN bucket
   getBucketObjects(bucketName: string) :Observable<S3Object[]> {
-    return this.http.get<S3Object[]>(`${this.apiUrlServer}/${bucketName}/objects`);
+    return this.http.get<S3Object[]>(`${this.apiUrlVPS}/${bucketName}/objects`);
   }
 
 }
